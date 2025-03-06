29a6a14dffc66a0b89dfdead8ea1ffd9ccd04fe4
Pranay <pranay@sanskartechnolab.com>
2025-03-06 15:53:46 +0530

    Commiting after Completed Reports

--- airplane_mode/airplane_mode/doctype/airplane/airplane.js
+++ airplane_mode/airplane_mode/doctype/airplane/airplane.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2025, Sanskar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airplane", {
+	refresh(frm) {
+        if(!frappe.user.has_role("Airport Authority Personnel")){
+            frm.set_df_property("initial_audit_completed","hidden",1)
+        }else{
+            frm.set_df_property("initial_audit_completed","hidden",0)
+        }
+	},
+});
--- airplane_mode/airplane_mode/doctype/airport/airport.js
+++ airplane_mode/airplane_mode/doctype/airport/airport.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2025, Sanskar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airport", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airport", {
+	refresh(frm) {
+        if(!frappe.user.has_role("Airport Authority Personnel")){
+            frm.set_df_property("initial_audit_completed","hidden",1)
+        }else{
+            frm.set_df_property("initial_audit_completed","hidden",0)
+        }
+	},
+});
